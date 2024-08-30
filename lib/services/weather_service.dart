dadd98a4fe4f1530f0cd4c6e129a177bd58b735f
James Seton <jamesseton6@gmail.com>
2024-08-30 10:00:56 +0930

    Further cleanup

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -7,7 +7,7 @@ import '../models/weather_model.dart';
 import 'package:http/http.dart' as http;
 
 class WeatherService {
-  static const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+  static const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
   Future getApiKey() async {
     await dotenv.load(fileName: 'key.env');
@@ -19,7 +19,7 @@ class WeatherService {
 
   Future<Weather> getWeather(String cityName) async {
     final response = await http
-        .get(Uri.parse('$BASE_URL?q=$cityName&appid=$apiKey&units=metric'));
+        .get(Uri.parse('$baseUrl?q=$cityName&appid=$apiKey&units=metric'));
 
     if (response.statusCode == 200) {
       return Weather.fromJson(jsonDecode(response.body));
