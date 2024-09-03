a132a21f6c4bdb9ce0229172b863853bc3f1e0d5
James Seton <jamesseton6@gmail.com>
2024-09-03 09:58:43 +0930

    implementing a donate button

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
+
+  _launchUrl() async {
+    //parsing url to uri
+    final Uri url = Uri.parse("https://ko-fi.com/jymmyboi");
+
+    if (!await launchUrl(url)) {
+      throw Exception('Could not launch $url');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Settings"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            ElevatedButton(
+              onPressed: _launchUrl,
+              child: Text("Coffee"),
+            ),
+          ],
+        ),
       ),
     );
   }
