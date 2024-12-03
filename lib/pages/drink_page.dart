aa9e39395f3a4ed8535c804d4eea7aad0afc4f86
James Seton <jamesseton6@gmail.com>
2024-12-03 14:21:08 +1030

    More formatting

--- lib/pages/drink_page.dart
+++ lib/pages/drink_page.dart
@@ -32,35 +32,42 @@ class DrinkPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(_drinkName),
+        title: Text(
+          _drinkName,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(11.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text(
-              _drinkDescription,
-              style: const TextStyle(fontSize: 20),
-            ),
-            ElevatedButton(
-              style: const ButtonStyle(
-                  backgroundColor:
-                      WidgetStatePropertyAll(Color.fromARGB(255, 0, 121, 107))),
-              onPressed: _launchUrl,
-              child: const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.attach_money_rounded,
-                    color: Color.fromARGB(255, 255, 255, 255),
-                  ),
-                  Text(
-                    "Buy this drink!",
-                    style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
-                  ),
-                ],
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text(
+                _drinkDescription,
+                style: const TextStyle(fontSize: 20),
               ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shape: const CircleBorder(),
+                        padding: const EdgeInsets.all(35),
+                        backgroundColor: const Color.fromARGB(255, 0, 121, 107),
+                        elevation: 5),
+                    onPressed: _launchUrl,
+                    child: const Icon(
+                      Icons.shopping_cart_rounded,
+                      color: Color.fromARGB(255, 255, 255, 255),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
